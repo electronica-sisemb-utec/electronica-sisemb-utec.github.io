557d66c81a928de27a6873f53503a6b1003b6789
Jhonatan Macazana <jhonatan.macazana@utec.edu.pe>
2020-04-17 01:22:49 -0500

    new page

---
+++ precache-manifest.fb95b2d8a76bd1b2e929aed12fe72193.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70032582b149c21f50f480f3c7ab355c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4547d09b8996a3a52a5",
+    "url": "/static/css/main.fb8f23b1.chunk.css"
+  },
+  {
+    "revision": "f104ff94823bf9e96642",
+    "url": "/static/js/2.f0e36759.chunk.js"
+  },
+  {
+    "revision": "e4547d09b8996a3a52a5",
+    "url": "/static/js/main.1752c3ca.chunk.js"
+  },
+  {
+    "revision": "c9e8088721d0339c3087",
+    "url": "/static/js/runtime~main.3f06ecf2.js"
+  },
+  {
+    "revision": "b6a7189f2fb57c438d703055642397d8",
+    "url": "/static/media/audifono.b6a7189f.jpg"
+  },
+  {
+    "revision": "1c428c68cce4598c8784213df8d584ec",
+    "url": "/static/media/bci.1c428c68.png"
+  },
+  {
+    "revision": "72eedbd441f0e10a9aa891e8c765ba9c",
+    "url": "/static/media/bebedero.72eedbd4.jpg"
+  },
+  {
+    "revision": "b1a207f9167a6d8ce85c54a31c0d99fb",
+    "url": "/static/media/bg-fpga.b1a207f9.jpg"
+  },
+  {
+    "revision": "baf6b40a654b078399e93e3d9cb6d455",
+    "url": "/static/media/bg-iot.baf6b40a.jpg"
+  },
+  {
+    "revision": "2702be97a2707ce4ce3b0050b1edbbca",
+    "url": "/static/media/bg-micro.2702be97.jpg"
+  },
+  {
+    "revision": "6f7e1fa2a1cefab345a831f5ee1459e1",
+    "url": "/static/media/brain.6f7e1fa2.jpg"
+  },
+  {
+    "revision": "55d5ad1accab1b20ae8e924309642bbf",
+    "url": "/static/media/carrito-iot.55d5ad1a.jpg"
+  },
+  {
+    "revision": "ba24dac72626063963f23c45f387826d",
+    "url": "/static/media/hobo.ba24dac7.jpeg"
+  },
+  {
+    "revision": "9d492901cfae7a0e39075f5ab638a839",
+    "url": "/static/media/idea.9d492901.jpg"
+  },
+  {
+    "revision": "c68f1c548fb25e2328a4fbb2fba4f937",
+    "url": "/static/media/landing-cas.c68f1c54.jpg"
+  },
+  {
+    "revision": "81175f3288ce89d00ca6112f11fae148",
+    "url": "/static/media/landing-circuits.81175f32.jpg"
+  },
+  {
+    "revision": "8bc0f861e3cf0c41edf92bf0c3a2d1c5",
+    "url": "/static/media/lifi.8bc0f861.jpg"
+  },
+  {
+    "revision": "b763df51fd482f3e235cdc0b25df5cd5",
+    "url": "/static/media/main-fpga.b763df51.jpg"
+  },
+  {
+    "revision": "b62d2b58a246eed9d05c34bf2d9273e0",
+    "url": "/static/media/main-iot.b62d2b58.svg"
+  },
+  {
+    "revision": "37dcc2d7733a0bb3c95c47c3dc30d308",
+    "url": "/static/media/main-micro.37dcc2d7.jpg"
+  },
+  {
+    "revision": "6434c7ce89587adc16fcca3334a0a0fa",
+    "url": "/static/media/mips.6434c7ce.jpg"
+  },
+  {
+    "revision": "7d1e033e593fb009b20aac4240c05b05",
+    "url": "/static/media/pcb.7d1e033e.jpg"
+  },
+  {
+    "revision": "bc3f9601d9454f00ff9b763fb31121ce",
+    "url": "/static/media/ph.bc3f9601.jpg"
+  },
+  {
+    "revision": "a72faeefa36df75f9ef219f91ac89394",
+    "url": "/static/media/pinguino.a72faeef.jpg"
+  },
+  {
+    "revision": "81908a18db030738f641406d87a5c804",
+    "url": "/static/media/rios.81908a18.jpg"
+  },
+  {
+    "revision": "898fa0cea6405cf5345bffa2c3b4fac2",
+    "url": "/static/media/skate.898fa0ce.jpg"
+  },
+  {
+    "revision": "108eb1d6f3de3a90ff61d560a1104d40",
+    "url": "/static/media/temperatura-iot.108eb1d6.jpeg"
+  },
+  {
+    "revision": "75866765b20d85440ec44945f7e4e4e1",
+    "url": "/static/media/tv-iot.75866765.jpg"
+  }
+]);
